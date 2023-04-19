25628a08342631e50aafb6fc530ee11325278f07
23r0 <125326946+GustavoAZepeda@users.noreply.github.com>
2023-04-19 15:49:27 -0600

    Update ldrSensor.ino

--- Proyecto2/ldrSensor.ino
+++ Proyecto2/ldrSensor.ino
@@ -1,28 +1,42 @@
 const int LRD_PIN = A0; // Pin analógico utilizado para el sensor LRD
 const int BOTON_PIN = 13; // Pin digital utilizado para el botón
+const int LED_PIN = 12; // Pin digital utilizado para el LED
 bool leerSensor = false; // Variable para indicar si se debe leer el sensor
 bool ultimoEstadoBoton = false; // Variable para almacenar el último estado del botón
 
 void setup() {
   Serial.begin(9600);
   pinMode(BOTON_PIN, INPUT_PULLUP); // Se establece el pin del botón como entrada con resistencia pull-up interna
+  pinMode(LED_PIN, OUTPUT); // Se establece el pin del LED como salida
 }
 
 void loop() {
-  // Se lee el estado actual del botón
-  bool estadoBoton = digitalRead(BOTON_PIN);
+  bool estadoBoton = leerEstadoBoton(); // Se lee el estado actual del botón
+  actualizarLecturaSensor(estadoBoton); // Se actualiza la variable leerSensor
+  leerSensorLRD(); // Se lee el valor del sensor LRD si corresponde
+  controlarLED(); // Se controla el LED según el estado del sensor
+}
 
-  // Si el estado del botón cambió, se actualiza la variable leerSensor
+bool leerEstadoBoton() {
+  bool estado = digitalRead(BOTON_PIN);
+  delay(50); // Se añade un pequeño retraso para evitar lecturas erróneas debido a rebotes del botón
+  return estado;
+}
+
+void actualizarLecturaSensor(bool estadoBoton) {
   if (estadoBoton != ultimoEstadoBoton && estadoBoton == LOW) {
     leerSensor = !leerSensor;
   }
-
-  // Se almacena el estado actual del botón
   ultimoEstadoBoton = estadoBoton;
+}
 
-  // Si la variable leerSensor es true, se lee el valor del sensor LRD y se imprime
+void leerSensorLRD() {
   if (leerSensor) {
     int valor = analogRead(LRD_PIN);
     Serial.println(valor);
   }
+}
+
+void controlarLED() {
+  digitalWrite(LED_PIN, !leerSensor); // Se enciende el LED cuando el sensor no está leyendo
 }
